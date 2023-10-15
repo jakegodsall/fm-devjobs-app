a17e18fa8cbb61805b6a8f30792c7c1afd0ff361
jakegodsall <jake.edward.godsall@gmail.com>
2023-10-15 11:58:23 +0200

    Added Spring MVC backend

---
+++ fm-devjobs-app-backend/fm-devjobs-app/src/main/java/com/jakegodsall/fmdevjobsapp/FmDevjobsAppApplication.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.fmdevjobsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmDevjobsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmDevjobsAppApplication.class, args);
+    }
+
+}
---
+++ fm-devjobs-app-backend/fm-devjobs-app/src/test/java/com/jakegodsall/fmdevjobsapp/FmDevjobsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.fmdevjobsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmDevjobsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
